5bffbcbfee28c1350e1c93f08916dd21b23c0c4e
caiyong <you@example.com>
2017-04-10 10:30:40 +0800

    。。

--- MyApplication/app/src/main/java/com/example/cy/myapplication/activity/SensorActivity.java
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/activity/SensorActivity.java
@@ -1,25 +1,61 @@
 package com.example.cy.myapplication.activity;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.cy.myapplication.R;
+import com.example.cy.myapplication.util.RotateSensorListener;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * 手机旋转监听
  */
 public class SensorActivity extends AppCompatActivity {
 
+    @Bind(R.id.tv_qianfan)
+    TextView tvQianfan;
+    @Bind(R.id.tv_houfan)
+    TextView tvHoufan;
+    @Bind(R.id.tv_zuofan)
+    TextView tvZuofan;
+    @Bind(R.id.tv_youfan)
+    TextView tvYoufan;
+    @Bind(R.id.tv_shunfan)
+    TextView tvShunfan;
+    @Bind(R.id.tv_nifan)
+    TextView tvNifan;
+
+
+    //手机转圈传感器监听
+    RotateSensorListener rotateSensorListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+        ButterKnife.bind(this);
+        rotateSensorListener = new RotateSensorListener(this) {
+            @Override
+            public void onQianfan(int num) {
+                tvQianfan.setText(num+"");
+            }
 
-        setRotateSensorListener(this);
+            @Override
+            public void onHoufan(int num) {
+                tvHoufan.setText(num+"");
+            }
+        };
+        rotateSensorListener.register();
     }
 
-    private void setRotateSensorListener(Context context) {
-        context.getSystemService(Context.SENSOR_SERVICE);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        rotateSensorListener.unRegister();
     }
 }
--- MyApplication/app/src/main/java/com/example/cy/myapplication/util/RotateSensorListener.java
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/util/RotateSensorListener.java
@@ -1,10 +1,130 @@
 package com.example.cy.myapplication.util;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
 /**
  * Created by joson on 2017/4/6.
  * 手机旋转监听
  */
-public class RotateSensorListener {
+public abstract class RotateSensorListener {
 
+
+    Context context;
+    SensorManager sensorManager;
+    SensorEventListener sensorEventListener;
+
+
+    public RotateSensorListener(Context context) {
+        this.context = context;
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    public void register() {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(sensorEventListener = new SensorEventListener() {
+                                           @Override
+                                           public void onSensorChanged(SensorEvent event) {
+                                               try {
+                                                   onSensorEvent(event.values);
+                                               } catch (Exception e) {
+                                                   e.printStackTrace();
+                                               }
+                                           }
+
+                                           @Override
+                                           public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                                           }
+                                       },
+                sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_FASTEST
+        );
+    }
+
+    public void unRegister(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    int qianfanTag = 0;//(对应第二个返回值)  前空翻标志，旋转0度:0，旋转90度:1，旋转180度:2，旋转270度:3，旋转0度:0;
+    int qianfanNum = 0;//前翻圈数统计
+
+    int houfanTag = 0;
+    int houfanNum = 0;
+
+    void onSensorEvent(float[] eventValues) {
+        qianfan(eventValues[1]);
+        houfan(eventValues[1]);
+    }
+
+    private void qianfan(float value) {
+        if (Math.abs(value) <= 40) {
+            if (qianfanTag == 3) {
+                qianfanNum++;
+                onQianfan(qianfanNum);
+            }
+            qianfanTag = 0;
+
+        } else if (value <= 130 && value >= 50) {
+            if (qianfanTag == 0) {
+                qianfanTag = 1;
+            }else if (qianfanTag!=1){
+                qianfanTag = 0;
+            }
+
+        } else if (Math.abs(value) >= 140) {
+            if (qianfanTag == 1) {
+                qianfanTag = 2;
+            }else  if (qianfanTag!=2){
+                qianfanTag = 0;
+            }
+        } else if (value <= -50 && value >= -130) {
+            if (qianfanTag == 2) {
+                qianfanTag = 3;
+            }else  if (qianfanTag!=3){
+                qianfanTag = 0;
+            }
+        }
+
+
+    }
+
+    private void houfan(float value) {
+        if (Math.abs(value) <= 40) {
+            if (houfanTag == 3) {
+                houfanNum++;
+                onHoufan(houfanNum);
+            }
+            houfanTag = 0;
+
+        } else if (value <= -50 && value >= -130) {
+            if (houfanTag == 0) {
+                houfanTag = 1;
+            }else if (houfanTag!=1){
+                houfanTag = 0;
+            }
+
+        } else if (Math.abs(value) >= 140) {
+            if (houfanTag == 1) {
+                houfanTag = 2;
+            }else  if (houfanTag!=2){
+                houfanTag = 0;
+            }
+        } else if (value >= 50 && value <= 130) {
+            if (houfanTag == 2) {
+                houfanTag = 3;
+            }else  if (houfanTag!=3){
+                houfanTag = 0;
+            }
+        }
+
+
+    }
+
+    public abstract void onQianfan(int num);
+    public abstract void onHoufan(int num);
 
 }
