1d13d30add3d6db8f7173a815e67fd70e0542035
caiyong <caiyongok123@126.com>
2017-04-24 21:30:46 +0800

    ..

---
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/widget/view/CustomView.java
@@ -0,0 +1,39 @@
+package com.example.cy.myapplication.widget.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.cy.myapplication.R;
+
+/**
+ * Created by cy on 2017/4/23.
+ */
+
+public class CustomView extends ViewGroup {
+    Context context;
+
+    public CustomView(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        //LayoutInflater.from(context).inflate(R.layout.activity_gif,this);
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        Toast.makeText(context, changed + "***" + l + "***" + t + "***" + r + "***" + b, 1).show();
+        Log.e("xxxxxxxxxxxxxxxxxxxx",changed + "***" + l + "***" + t + "***" + r + "***" + b);
+    }
+
+
+}
