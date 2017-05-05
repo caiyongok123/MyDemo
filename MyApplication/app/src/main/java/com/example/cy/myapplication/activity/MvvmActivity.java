c83e3bdbc7ffd69fa0e2eec333b21bf98acb864d
caiyong <you@example.com>
2017-05-05 18:23:19 +0800

    。。

--- MyApplication/app/src/main/java/com/example/cy/myapplication/activity/MvvmActivity.java
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/activity/MvvmActivity.java
@@ -1,9 +1,8 @@
 package com.example.cy.myapplication.activity;
 
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.cy.myapplication.R;
 import com.example.cy.myapplication.databinding.ActivityMvvmBinding;
@@ -19,14 +18,16 @@ public class MvvmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMvvmBinding vdb = DataBindingUtil.setContentView(this,R.layout.activity_mvvm);
         Entity en = new Entity();
-        en.name = "狗屎强";
+        //en.name = "狗屎强";
         vdb.setPpa(en);
-        en.setName("123");
+        //en.name="123";
     }
 
 
     public class Entity {
+        public boolean isAdult = true;
         public String name;
+        public String name2 = "222222222222222222";
 
         public String getName() {
             return name;
