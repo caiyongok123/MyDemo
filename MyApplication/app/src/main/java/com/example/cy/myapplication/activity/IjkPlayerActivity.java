d2251ab306a7c480d347598f54c30fafc8162499
caiyong <you@example.com>
2017-05-20 16:02:10 +0800

    。。

--- MyApplication/app/src/main/java/com/example/cy/myapplication/activity/IjkPlayerActivity.java
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/activity/IjkPlayerActivity.java
@@ -1,7 +1,6 @@
 package com.example.cy.myapplication.activity;
 
 import android.databinding.DataBindingUtil;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +8,8 @@ import com.example.cy.myapplication.R;
 import com.example.cy.myapplication.databinding.ActivityIjkPlayerBinding;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 /**
  * B站IJK播放器播放视频
@@ -17,18 +18,21 @@ import java.io.IOException;
 public class IjkPlayerActivity extends AppCompatActivity {
 
     ActivityIjkPlayerBinding binding;
-    IjkVideoView
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_ijk_player);
-        try {
+        /*try {
             binding.vv.setDataSource("http://125.71.238.129:7086/Files/MVideo/20160118nx.mp4");
             binding.vv.start();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+
 
     }
 }
