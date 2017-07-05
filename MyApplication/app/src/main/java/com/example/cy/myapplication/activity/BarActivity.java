b821fcb814bad16c0a61180e6c18f5972fe6dc88
caiyong <you@example.com>
2017-07-05 18:00:06 +0800

    ..

--- MyApplication/app/src/main/java/com/example/cy/myapplication/activity/BarActivity.java
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/activity/BarActivity.java
@@ -1,7 +1,6 @@
 package com.example.cy.myapplication.activity;
 
-import android.app.Activity;
-import android.graphics.Color;
+import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,47 +18,42 @@ public class BarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //透明状态栏
-        if (Build.VERSION.SDK_INT>=19){
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
+        setTranslucentStatusBar();
         // 全屏和状态栏设置为浅色模式(去除半透明色，文字为黑色（默认为半透明色，文字为白色）)
-        if (Build.VERSION.SDK_INT>=23){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
+        setFullScreenAndLightStatusBar();
 
         setContentView(R.layout.activity_bar);
-
-        {   //使布局不会顶到屏幕顶部(不与状态栏重叠)，(必须在setContentView 方法执行后才生效)
-            ViewGroup contentFrameLayout = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
-            View parentView = contentFrameLayout.getChildAt(0);
-            if (parentView != null && Build.VERSION.SDK_INT >= 14) {
-                parentView.setFitsSystemWindows(true);
-            }
-        }
+        setFitsSystemWindows();
 
 
     }
 
-    private void initStatusBar() {
-        Window win = getWindow();
-        if (1==1) {
-            win.requestFeature(Window.FEATURE_NO_TITLE);
-            win.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
-            win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);// 保持屏幕高亮
-        } else {
-            //KITKAT也能满足，只是SYSTEM_UI_FLAG_LIGHT_STATUS_BAR（状态栏字体颜色反转）只有在6.0才有效
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                win.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//透明状态栏
-                // 状态栏字体设置为深色，SYSTEM_UI_FLAG_LIGHT_STATUS_BAR 为SDK23增加
-                win.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
-                // 部分机型的statusbar会有半透明的黑色背景
-                win.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                win.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                win.setStatusBarColor(Color.TRANSPARENT);// SDK21
+    /**
+     * 设置半透明状态栏
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public void setTranslucentStatusBar(){
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
 
-                //isStatusBarTranslate = true;
-            }
-        }
+    /**
+     * *********这个方法在有些手机上无效********
+     * 设置全屏 和 设置状态栏为浅色模式(去除半透明色，文字为黑色（默认为半透明色，文字为白色）)
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    public void setFullScreenAndLightStatusBar(){
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+
+
+    /**
+     * 使布局不会顶到屏幕顶部(不与状态栏重叠)，(必须在setContentView 方法执行后才生效)
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void setFitsSystemWindows(){
+        ViewGroup contentFrameLayout = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
+        View parentView = contentFrameLayout.getChildAt(0);
+        parentView.setFitsSystemWindows(true);
     }
 }
