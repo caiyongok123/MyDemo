c49b7e5f11488ea4665ccef460b878ec0aa3b318
caiyong <you@example.com>
2017-07-17 11:32:29 +0800

    ..

--- MyApplication/app/src/main/java/com/example/cy/myapplication/activity/CameraPreviewActivity.java
+++ MyApplication/app/src/main/java/com/example/cy/myapplication/activity/CameraPreviewActivity.java
@@ -11,12 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.cy.myapplication.R;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -70,7 +76,11 @@ public class CameraPreviewActivity extends AppCompatActivity {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-
+                try {
+                    mCamera.setPreviewDisplay(null);
+                    mCamera.stopPreview();
+                } catch (Exception e) {
+                }
             }
         });
 
@@ -84,16 +94,29 @@ public class CameraPreviewActivity extends AppCompatActivity {
                 new Camera.PictureCallback() {//jepg回调
                     @Override
                     public void onPictureTaken(final byte[] data, Camera camera) {
+                        mCamera.startPreview();
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
-                                File f = new File(Environment.getExternalStorageDirectory().getPath() + "/111111.jpg");
+
+                                try {
+                                    final String log = openUrl(data);
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(MyApplication.myApplication,log,Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                /*File f = new File(Environment.getExternalStorageDirectory().getPath() + "/111111.jpg");
                                 try {
                                     FileOutputStream os = new FileOutputStream(f);
                                     os.write(data);
                                     os.close();
                                     ExifInterface exifInterface = new ExifInterface(Environment.getExternalStorageDirectory().getPath() + "/111111.jpg");
-                                    exifInterface.setAttribute(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_ROTATE_90+"");
+                                    exifInterface.setAttribute(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_ROTATE_90 + "");
                                     exifInterface.saveAttributes();
 
                                 } catch (IOException e) {
@@ -104,19 +127,58 @@ public class CameraPreviewActivity extends AppCompatActivity {
                                     public void run() {
                                         mCamera.startPreview();
                                     }
-                                });
+                                });*/
                             }
                         }).start();
                     }
                 });
     }
 
-    //快门按下的时候onShutter()被回调
-    private Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
-        public void onShutter() {
-            //发出提示用户的声音
 
+    class ImgUploader {
+    }
+
+
+    public static String openUrl(byte[] data) throws Exception {
+        URL urls = new URL("http://52.33.105.73:5000/predict/upload");
+        HttpURLConnection connection = null;
+        OutputStream outputStream = null;
+        String rs = null;
+        try {
+            connection = (HttpURLConnection) urls.openConnection();
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=----111111111111111111111111");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setConnectTimeout(8000);
+            connection.setReadTimeout(20000);
+            connection.setRequestMethod("POST");
+
+            outputStream = connection.getOutputStream();
+            outputStream.write(data);
+            try {
+                connection.connect();
+                if (connection.getResponseCode() == 200) {
+                    rs = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+                }
+            } catch (Exception e) {
+                rs = null;
+            }
+
+
+
+            return rs;
+        } finally {
+            try {
+                outputStream.close();
+            } catch (Exception e) {
+            }
+            outputStream = null;
+
+            if (connection != null)
+                connection.disconnect();
+            connection = null;
         }
-    };
+
+    }
 
 }
